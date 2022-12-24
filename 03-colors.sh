77e18a5c93bb8e3947b52800d14dda55ae6574de
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-24 14:39:36 +0530

    added colors

--- 03-colors.sh
+++ 03-colors.sh
@@ -7,3 +7,4 @@ echo -e "\e[34m I am printing Blue Color\e[0m"
 echo -e "\e[35m I am printing Cyan Color\e[0m"
 
 echo -e "\e[41;33m Welcome to colors\e[0m"
+echo -e "\e[47;32m Welcome to colors\e[0m"
