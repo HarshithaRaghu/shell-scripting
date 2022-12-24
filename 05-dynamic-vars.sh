c7eb1d75e4ac4478289f329b6e2cf1f32a784036
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-24 17:03:05 +0530

    added variables

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -4,3 +4,7 @@ DATE="24-12-2022"
 DATE=$(date +%F)
 
 echo "GoodMorning todays date is $DATE"
+echo "Total number of opened sessions is $(who | wc -l)"
+
+echo -e "Total number of opened sessions is \e[31m $(who | wc -l) \e[0m"
+
