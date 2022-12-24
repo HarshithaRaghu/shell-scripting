c515f5b80de51487990d8d98483c694dffa2b0a3
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-24 13:22:16 +0530

    added agenda file

--- 01-sample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# SheBang Notation tells the interpretor what the code and based on that it will understand and compile/run the script
-# You can have only one SheBang Notation per script and should always be the first file or start of the script
-# Apart from the SheBang Notation anything that starts with '#' is referred as a comment
-
-# comments are just for hte human readable purpose only
-
-echo "Hello world"
