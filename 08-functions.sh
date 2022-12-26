f99984a394e07611ba166c909e7d2fb34a3a4936
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-26 21:01:44 +0530

    added functions

--- 08-functions.sh
+++ 08-functions.sh
@@ -12,3 +12,12 @@ sample
 
 #calling the function third time
 sample 
+
+stat() {
+    echo "number of opened sessions : (who | wc -l)"
+    echo "today's date is : $(date +%F)"
+    echo "load average on the system in last 1 minute :  uptime | awk -F : '{print $NF}' | awk -F , '{print $1}' "
+}
+
+#calling the function
+stat
