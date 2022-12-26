d7aa346026590429ca2d2f552dedd58b98ff8ff7
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-26 19:31:09 +0530

    added lines

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -3,12 +3,11 @@
 echo "Name of the script is : $0"
 echo "value of a is :$a"
 echo "value of b is :$b"
-echo "here are the variables used in the script : $*"
-echo "number of variables used in the script : $#"
 
 
-a=$1
-b=$2
+
+$a=$1
+$b=$2
 
 
 
