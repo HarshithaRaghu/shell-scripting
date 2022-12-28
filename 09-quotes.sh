da74629a73fedf27ea8cea8b62dcfae18847579a
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-28 12:15:39 +0530

    added case

---
+++ 09-quotes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+single quotes in echo will have the power of special variable, prefer to avoid single quotes when you get to print something using echo
+
+
---
+++ 10-case.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# case $var in
+#    opt1) commands1 ;;
+#    opt2) commands2 ;;
+# esac
+
+ACTION = $1
+
+# case $ACTION in
+#    start)
+#         echo "XYZ service is starting"
+#         exit 0
+#         ;;
+#    stop)
+#         echo "XYZ service is stopping"
+#         exit 0
+#         ;;      
+#    restart)
+#         echo "XYZ service is restarting"
+#         exit 0
+#         ;;
+#     *)
+#         echo -e "\e[31m valid options are start or stop or resstart only \e[0m"
