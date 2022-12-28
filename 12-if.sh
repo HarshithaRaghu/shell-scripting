4f0ad5da5afeca6e7b53fac959b2851d974fb574
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-28 18:20:02 +0530

    added comments

--- 12-if.sh
+++ 12-if.sh
@@ -8,11 +8,11 @@ fi
 
 # Demo on else if
 c=$1
-if [ "$c" -eq "10" ]; then
+if [ "$c" -eq "10" ] ; then
     echo "value is c 10"
-elif [ "$c" -eq "20" ]; then
+elif [ "$c" -eq "20" ] ; then
     echo "value is c 30"
-elif [ "$c" -eq "30" ]; then
+elif [ "$c" -eq "30" ] ; then
     echo "value is c 30"
 else 
     echo "value if not 10 or 20 or 30"
