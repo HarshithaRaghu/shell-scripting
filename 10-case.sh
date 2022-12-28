cc21acc59bb1c6b1b9c0fa0ee0b8366d00567a7d
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-28 12:35:04 +0530

    added case

--- 10-case.sh
+++ 10-case.sh
@@ -4,20 +4,20 @@
 #    opt2) commands2 ;;
 # esac
 ACTION=$1
-# case $ACTION in
-#    start)
-#         echo "XYZ service is starting"
-#         exit 0
-#         ;;
-#    stop)
-#         echo "XYZ service is stopping"
-#         exit 0
-#         ;;      
-#    restart)
-#         echo "XYZ service is restarting"
-#         exit 0
-#         ;;
-#     *)
-#         echo -e "\e[31m valid options are start or stop or resstart only \e[0m"
-#         exit 1
-# esac
+case $ACTION in
+    start)
+         echo "XYZ service is starting"
+         exit 0
+         ;;
+    stop)
+         echo "XYZ service is stopping"
+         exit 0
+         ;;      
+    restart)
+         echo "XYZ service is restarting"
+         exit 0
+         ;;
+     *)
+         echo -e "\e[31m valid options are start or stop or resstart only \e[0m"
+         exit 1
+ esac
