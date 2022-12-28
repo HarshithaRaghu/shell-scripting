5da585e168a6cecb1c1fd55b12358e8a3a5a26c7
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-28 19:09:53 +0530

    added comments

--- 13-loops.sh
+++ 13-loops.sh
@@ -2,4 +2,9 @@
 
 for list in val1 val2 val3 val4 val5; do
      echo value is $list
-done     
+done  
+
+for courses in devops aws gcp azure terraform ansible docker; do
+     echo course name is $courses 
+done 
+
