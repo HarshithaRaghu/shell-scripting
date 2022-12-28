5288e26b2b3b26c3ce47645ac055bf435f995349
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-28 21:15:35 +0530

    added commands

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo I am frontend
+UID=$(id -u)
+if [ $UID -ne 0 ] ; then
+   echo -e "\e[31m you need to yun script either as a root user or with a sudo privilege\e[0m"
+   exit 1
+if 
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
