574a665b629f44176d0169f04bcb567881061819
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-28 18:16:01 +0530

    added comments

--- 12-if.sh
+++ 12-if.sh
@@ -6,4 +6,15 @@ if [ "$a" == "XYZ" ]; then
    echo -e "\e[32m Both of them are equal \e[0m"
 fi
 
+# Demo on else if
+c=$1
+if ["$c" -eq "10"]; then
+    echo "value is c 10"
+elif ["$c" -eq "20"]; then
+    echo "value is c 30"
+elif ["$c" -eq "30"]; then
+    echo "value is c 30"
+else 
+    echo "value if not 10 or 20 or 30"
+fi    
   
