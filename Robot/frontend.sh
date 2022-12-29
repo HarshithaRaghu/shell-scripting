ef4d8a521ba467453177535e15fc894a5120196c
HarshithaRaghu <harshithadarshan95@gmail.com>
2022-12-29 10:43:21 +0530

    added lines

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -9,4 +9,4 @@ fi
 
 yum install nginx -y    &>> /tmp/frontend.log
 systemctl enable nginx  &>> /tmp/frontend.log
-systemctl start nginx   &>> /tmp/frontend.log
+systemctl start nginx   &>> /tmp/frontend.log 
