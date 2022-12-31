8c4e984db780437112af62d7fc537b86b0a7f033
Harshitha <harshithadarshan95@gmail.com>
2022-12-31 10:58:10 -0800

    added

--- Robot/mysql.sh
+++ Robot/mysql.sh
@@ -32,6 +32,6 @@ fi
 echo show plugins | mysql -uroot -pRoboShop@1 | grep validate_password;   &>> "${LOGFILE}"
 if [ $? -eq 0 ]; then 
     echo -n "Uninstalling Password Validate Plugin "
-    echo "uninstall plugin validate_password;"|  mysql -uroot -pRoboShop@1 | grep validate_password  &>> "${LOGFILE}"
+    echo "show plugins;"|  mysql -uroot -pRoboShop@1 | grep validate_password  &>> "${LOGFILE}"
     stat $?
 fi 
