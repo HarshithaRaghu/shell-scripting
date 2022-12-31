a1342b1f53ed8aed537109d09c60b6e9a004836e
Harshitha <harshithadarshan95@gmail.com>
2022-12-30 17:15:57 -0800

    added

--- Robot/catalogue.sh
+++ Robot/catalogue.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo I am catalogue
+set -e 
+COMPONENT=mongodb
+
+source common.sh
+
+echo -n "Configuring Nodejs repo: "
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y  &>> "${LOGFILE}"
+stat $?
+
+
+
