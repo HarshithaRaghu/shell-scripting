155dc046b005e14ce7de713c13ea7b367df2af92
Harshitha <harshithadarshan95@gmail.com>
2022-12-31 03:13:29 -0800

    added

--- Robot/catalogue.sh
+++ Robot/catalogue.sh
@@ -10,10 +10,12 @@ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>
 yum install nodejs -y  &>> "${LOGFILE}"
 stat $?
 
+id $APPUSER &>> "${LOGFILE}"
+if [ $? -ne 0 ] ; then 
 echo -n "creating application user $APPUSER :"
 useradd $APPUSER  &>> "${LOGFILE}"  
 stat $?
-
+fi 
 echo -n "downloading the $COMPONENT :"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
 stat $?
