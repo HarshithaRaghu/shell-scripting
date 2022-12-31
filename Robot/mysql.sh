b682003a20654eb3728777529eff41f4e01a27ae
Harshitha <harshithadarshan95@gmail.com>
2022-12-31 08:46:00 -0800

    added

--- Robot/mysql.sh
+++ Robot/mysql.sh
@@ -4,15 +4,16 @@ COMPONENT=mysql
 
 source Robot/common.sh
 
-echo -n "downloading the $COMPONENT : "
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> "${LOGFILE}"
-stat $?
+echo -n "Configuring the $COMPONENT repo  : "
+curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo  &>> "${LOFGILE}"
+stat $? 
 
-echo -n "Installing $COMPONENT : "
-yum install mysql-community-server -y 
-stat $?
+echo -n "Installing $COMPONENT: "
+yum install mysql-community-server -y   &>> "${LOFGILE}"
+stat $? 
 
-echo -n "starting $COMPONENT : "
-systemctl enable mysqld 
+echo -n "Starting $COMPONENT : "
+systemctl enable mysqld
 systemctl start mysqld 
-stat $?
+stat $? 
+
