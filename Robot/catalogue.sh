ffd722546f7c5083aa5307c15ae714132350ee71
Harshitha <harshithadarshan95@gmail.com>
2022-12-31 01:27:50 -0800

    added

--- Robot/catalogue.sh
+++ Robot/catalogue.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 COMPONENT=catalogue
 
@@ -11,9 +10,6 @@ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>
 yum install nodejs -y  &>> "${LOGFILE}"
 stat $?
 
-echo -n "creating application user $APPUSER :"
-useradd $APPUSER   &>> "${logfile}"
-stat $?
 
 
 
