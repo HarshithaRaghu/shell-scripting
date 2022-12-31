9a0e5ff4cf1ff9c9a80ed5964e951eeb52c66f97
Harshitha <harshithadarshan95@gmail.com>
2022-12-30 17:41:15 -0800

    added

--- Robot/mongodb.sh
+++ Robot/mongodb.sh
@@ -2,21 +2,7 @@
 
  COMPONENT=mongodb
 
-LOGFILE="/tmp/$COMPONENT.log"
-
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-   echo -e "\e[31m you need to run script either as a root user or with a sudo privilege\e[0m"
-   exit 1
-fi
-
-stat () {
-   if [ $1 -eq 0 ]; then
-    echo -e "\e[32m success \e[0m"
-   else
-    echo -e "\e[31m failure \e[0m"
-   fi      
-}
+source common.sh
 
 echo -n "downloading the $COMPONENT : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
