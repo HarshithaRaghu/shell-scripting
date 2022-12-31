a235485c730d70a7b35474fc6129cf43b4dd2a8b
Harshitha <harshithadarshan95@gmail.com>
2022-12-30 17:56:13 -0800

    added

--- Robot/catalogue.sh
+++ Robot/catalogue.sh
@@ -2,32 +2,17 @@
 
 COMPONENT=catalogue
 
-LOGFILE="/tmp/$COMPONENT.log"
-
-APPUSER=roboshop
-
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-   echo -e "\e[31m you need to run script either as a root user or with a sudo privilege\e[0m"
-   exit 1
-fi
-
-stat () {
-   if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
-   else
-    echo -e "\e[31m failure \e[0m"
-   fi      
-} 
+source Robot/common.sh
 
 echo -n "Configuring Nodejs repo: "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> "${LOGFILE}"
 yum install nodejs -y  &>> "${LOGFILE}"
 stat $?
 
 echo -n "creating application user $APPUSER :"
-useradd $APPUSER 
+useradd $APPUSER &>> "${LOGFILE}"
 stat $?
+
 
 
 
