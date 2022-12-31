31916310accec936a13fc375f16235fbbaefd97e
Harshitha <harshithadarshan95@gmail.com>
2022-12-31 02:07:34 -0800

    added

--- Robot/catalogue.sh
+++ Robot/catalogue.sh
@@ -14,12 +14,12 @@ echo -n "creating application user roboshop :"
 useradd roboshop &>> "${LOGFILE}" 
 stat $?
 
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/catalogue.zip
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
-$ npm install
+# $ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+# $ cd /home/roboshop
+# $ unzip /tmp/catalogue.zip
+# $ mv catalogue-main catalogue
+# $ cd /home/roboshop/catalogue
+# $ npm install
 
 
 
