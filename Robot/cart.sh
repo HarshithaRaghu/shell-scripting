760a5c61d74fd6a9e3dcb69a39ad0f531d1d74ab
Harshitha <harshithadarshan95@gmail.com>
2022-12-31 06:41:56 -0800

    added

--- Robot/cart.sh
+++ Robot/cart.sh
@@ -1,3 +1,52 @@
 #!/bin/bash
 
-echo I am cart
+COMPONENT=catalogue
+
+source Robot/common.sh 
+
+
+echo -n "Configuring and installing Nodejs repo: "
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> "${LOGFILE}"
+yum install nodejs -y &>> "${LOGFILE}"
+stat $?
+
+id $APPUSER &>> "${LOGFILE}"
+if [ $? -ne 0 ] ; then 
+echo -n "creating application user $APPUSER :"
+useradd $APPUSER  &>> "${LOGFILE}"  
+stat $?
+fi 
+
+echo -n "downloading the $COMPONENT :"
+curl -s -L -o /tmp/cart.zip "https://github.com/stans-robot-project/cart/archive/main.zip"
+stat $?
+
+echo -n "cleanup and extracting the $COMPONENT: "
+rm -rf /home/$APPUSER/$COMPONENT/
+cd /home/$APPUSER
+unzip -o /tmp/cart.zip &>> "${LOGFILE}"
+stat $?
+
+echo -n "changing the ownership to $APPUSER: "
+mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
+chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+stat $?
+
+echo -n "installing $COMPONENT Dependencies: "
+cd $COMPONENT
+npm install &>> "${LOGFILE}"
+stat $?
+
+echo -n "configuring the $COMPONENT service: "
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service 
+stat $?
+
+echo -n "starting $COMPONENT service: "
+systemctl daemon-reload &>> "${LOGFILE}" 
+systemctl enable $COMPONENT &>> "${LOGFILE}"
+systemctl restart $COMPONENT &>> "${LOGFILE}"
+stat $?
+
+echo -n -e "\e[32m ________ $COMPONENT Configuration is starting ________ \e[0m"
+
